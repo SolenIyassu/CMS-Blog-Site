fdd52b8e3b18ca456b377704a7dabbd0b32cef83
SolenIyassu <Solen.Iyassu@gmail.com>
2022-09-26 18:30:37 -0600

    fixed env

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -16,7 +16,7 @@ router.put('/', (req, res)=>{
     try {
         const[effectsRows] = await Post.update(req.body, {
             where: {
-                id: req.param.id,
+                id: req.params.id,
             },
             });
             if (affectedRows > 0) { 
@@ -25,8 +25,27 @@ router.put('/', (req, res)=>{
             } else {
                 res.status(500).json({message:'upate has been nade!'})
             }
-        });
-    } catch (error) {
+        
+    } catch (err) {
         res.status(500).json({message:'could not be updated!'})
     }
-]);
+});
+router.delete('/:id', (req,res)=>{
+    try {
+        const postsData = await Post.destroy({
+            where:{
+                id: req.params.id,
+                user_id: req.session.user_id,
+            }
+        })
+        if(!postsData){
+            res.status(404).json({message:'Could not find post!'})
+            return;
+        }
+        res.status(200).json (postsData);
+    } catch (err) {
+        res.status(500).json (err)
+    }
+})
+
+module.exports = router;
